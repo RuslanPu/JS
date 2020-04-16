22f36ff7603feb9aff66975d991bb4340c05549f
Ruslan <ruslanpunin@yande.ru>
2020-04-16 17:10:50 +0300

    replace js file in resource/static/js/js

--- src/main/java/com/javamentor/js/JsApplication.java
+++ src/main/java/com/javamentor/js/JsApplication.java
@@ -2,12 +2,18 @@ package com.javamentor.js;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class JsApplication {
+public class JsApplication implements WebMvcConfigurer {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(JsApplication.class, args);
     }
+
+
 
 }
